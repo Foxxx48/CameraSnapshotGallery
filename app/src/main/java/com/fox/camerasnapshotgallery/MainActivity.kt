aece9304325a9dfb66b08cdf2284b765baebbce0
Foxxx48 <Foxxdim@mail.ru>
2022-08-12 13:22:25 +0300

    added possibility to save files

--- app/src/main/java/com/fox/camerasnapshotgallery/MainActivity.kt
+++ app/src/main/java/com/fox/camerasnapshotgallery/MainActivity.kt
@@ -4,12 +4,19 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.fox.camerasnapshotgallery.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -19,15 +26,17 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_IMAGE_CAPTURE = 1
     private val REQUEST_PICK_IMAGE = 2
 
+    lateinit var currentPhotoPath: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.btCapturePhoto.setOnClickListener {
             openCamera()
         }
+
         binding.btOpenGallery.setOnClickListener {
             openGallery()
         }
@@ -47,11 +56,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun openCamera() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
-            intent.resolveActivity(packageManager)?.also {
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
-            }
-        }
+        var myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                val photoFile: File? = try {
+                    createCapturedPhoto()
+                } catch (ex: IOException) {
+                    // If there is error while creating the File, it will be null
+                    null
+                }
+                photoFile?.also {
+                    val photoURI = FileProvider.getUriForFile(
+                        this,
+                        "${BuildConfig.APPLICATION_ID}.fileprovider",
+                        it
+                    )
+
+                    println(photoURI)
+                    myIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
+                }
+
+
     }
     private fun openGallery() {
         Intent(Intent.ACTION_GET_CONTENT).also { intent ->
@@ -61,16 +86,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    @Throws(IOException::class)
+    private fun createCapturedPhoto(): File {
+        val timestamp:String = SimpleDateFormat("yyyyMMdd-HHmmss-", Locale.US).format (Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("PHOTO_${timestamp}",".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
+
             if (requestCode == REQUEST_IMAGE_CAPTURE) {
-                val bitmap = data?.extras?.get("data") as Bitmap
-                binding.ivImage.setImageBitmap(bitmap)
+                val uri = Uri.parse(currentPhotoPath)
+                binding.ivImage.setImageURI(uri)
             }
             else if (requestCode == REQUEST_PICK_IMAGE) {
                 val uri = data?.getData()
                 binding.ivImage.setImageURI(uri)
+                println(uri)
             }
         }
     }
